35b86b89d45b644b9687ccea832ec5436294a21a
JeromeJaya <jeromeat2002@gmail.com>
2026-09-13 21:04:38 +0530

    chenges made

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
-    baseURL: "http://localhost:3001"||API_URL,
+    baseURL: API_URL,
 });
 
 export default api;
